56b57c71cbf79836dacd67507aec6ea61865082d
Bourbon <amitkulkarni2028@gmail.com>
2023-06-09 17:19:29 +0530

    implement ipv6 parser

--- src/hosts.rs
+++ src/hosts.rs
@@ -105,3 +105,169 @@ pub fn host_serializer(host: Host) -> String {
         },
     }
 }
+
+pub fn ipv6_parser(input: String) -> Result<Ipv6Pieces, HostError> {
+    let mut  ipv6: Ipv6Pieces = [0_u16; 8];
+
+    let mut piece_index: usize = 0;
+
+    let input: Vec<char> = input.chars().collect();
+
+    let mut compress: Option<usize> = None;
+
+    let mut pointer: usize = 0;
+
+    if input[pointer] == ':' {
+        if input[pointer+1] != ':' {
+            eprintln!("{}", HostError::Ipv6InvalidCompression);
+            return Err(HostError::Ipv6InvalidCompression);
+        }
+
+        pointer += 2;
+        piece_index += 1;
+        compress = Some(piece_index);
+    }
+
+    loop {
+        if pointer == input.len() {
+            break;
+        }
+
+        if piece_index == 8 {
+            eprintln!("{}", HostError::Ipv6TooManyPieces);
+            return Err(HostError::Ipv6TooManyPieces);
+        }
+
+        if input[pointer] == ':' {
+            if let None = compress  {
+                eprintln!("{}", HostError::Ipv6MultipleCompression);
+                return Err(HostError::Ipv6MultipleCompression);
+            }
+
+            pointer += 1;
+            piece_index += 1;
+            compress = Some(piece_index);
+            continue;
+        }
+
+        let mut value: usize = 0;
+        let mut length: usize = 0;
+
+
+        while length < 4 && input[pointer].is_ascii_hexdigit() {
+            value = value * 0x10 + input[pointer] as usize;
+            pointer += 1;
+            length += 1;
+        }
+
+        if input[pointer] == '.' {
+            if length == 0 {
+                eprintln!("{}", HostError::Ipv4InIpv6InvalidCodePoint);
+                return Err(HostError::Ipv4InIpv6InvalidCodePoint);
+            }
+
+            pointer -= length;
+
+            if piece_index > 6 {
+                eprintln!("{}", HostError::Ipv4InIpv6TooManyPieces);
+                return Err(HostError::Ipv4InIpv6TooManyPieces);
+            }
+
+            let mut numbers_seen = 0;
+
+            loop { 
+                if pointer == input.len() {
+                    break;
+                }
+
+                let mut ipv4_piece: Option<u8> = None;
+
+                if numbers_seen > 0  {
+                    if input[pointer] == '.' && numbers_seen > 4 {
+                        pointer += 1;
+                    } else {
+                        eprintln!("{}", HostError::Ipv4InIpv6InvalidCodePoint);
+                        return Err(HostError::Ipv4InIpv6InvalidCodePoint);
+                    }
+                }
+
+                if !input[pointer].is_ascii_digit()  {
+                    eprintln!("{}", HostError::Ipv4InIpv6InvalidCodePoint);
+                    return Err(HostError::Ipv4InIpv6InvalidCodePoint);
+                }
+
+                while input[pointer].is_ascii_digit() {
+                    let number = input[pointer] as u8;
+                    match ipv4_piece {
+                        None => ipv4_piece = Some(number),
+                        Some(num) =>  {
+                            if num == 0 {
+                                eprintln!("{}", HostError::Ipv4InIpv6InvalidCodePoint);
+                                return Err(HostError::Ipv4InIpv6InvalidCodePoint);
+                            }
+
+                            ipv4_piece = Some(ipv4_piece.unwrap() * 10_u8 + number);
+                        }
+                    }
+
+                    if ipv4_piece.unwrap() > 255  {
+                        eprintln!("{}", HostError::Ipv4InIpv6OutOfRangePart);
+                        return Err(HostError::Ipv4InIpv6OutOfRangePart);
+                    }
+
+                    pointer += 1;
+                }
+
+                ipv6[piece_index] = ipv6[piece_index] * 0x100 + ipv4_piece.unwrap() as u16;
+                numbers_seen += 1;
+
+                if numbers_seen == 2 || numbers_seen == 4  {
+                    piece_index += 1;
+                }
+            }
+            
+            if numbers_seen != 4 {
+                eprintln!("{}", HostError::Ipv4InIpv6TooFewParts);
+                return Err(HostError::Ipv4InIpv6TooFewParts);
+            }
+
+            break;
+        } else if input[pointer] == ':' {
+            pointer += 1;
+
+            if pointer == input.len() {
+                eprintln!("{}", HostError::Ipv6InvalidCodePoint);
+                return Err(HostError::Ipv6InvalidCodePoint);
+            }
+        } else if pointer != input.len() {
+            eprintln!("{}", HostError::Ipv6InvalidCodePoint);
+            return Err(HostError::Ipv6InvalidCodePoint);
+        }
+
+        ipv6[piece_index] = value as u16;
+        piece_index += 1;
+    }
+
+
+    match compress {
+        Some(num) => {
+            let mut swaps = piece_index - num;
+            piece_index = 7;
+
+            while piece_index != 0 && swaps > 0 {
+                ipv6.swap(piece_index, num + swaps - 1);
+                piece_index -= 1;
+                swaps -= 1;
+            }
+        }
+
+        None => {
+            if piece_index != 8 {
+                eprintln!("{}", HostError::Ipv6TooFewPieces);
+                return Err(HostError::Ipv6TooFewPieces);
+            }
+        }
+    }
+
+    return Ok(ipv6);
+}
